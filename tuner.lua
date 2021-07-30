656dfaadbe364943f579fb59b90a53e85f7f2bd4
TimothyWhiteOC <47747244+TimothyWhiteOC@users.noreply.github.com>
2021-07-30 08:30:32 -0500

    Update tuner.lua

--- tuner.lua
+++ tuner.lua
@@ -1,66 +1,63 @@
-function randomInteger(max, min)
+function randomInteger(min, max)
 	math.randomseed(os.clock())
-	return math.floor(math.random() * (max-min) + min)
+	return math.random(min, max)
 end
 
 function normalize(candidate)
-	local norm = math.sqrt(candidate.heightWeight * candidate.heightWeight + 
-						   candidate.linesWeight * candidate.linesWeight + 
-						   candidate.holesWeight * candidate.holesWeight + 
-						   candidate.bumpinessWeight * candidate.bumpinessWeight)
-	candidate.heightWeight /= norm
-    candidate.linesWeight /= norm
-    candidate.holesWeight /= norm
-    candidate.bumpinessWeight /= norm
+	local norm = math.sqrt(candidate.heightWeight * candidate.heightWeight +
+							candidate.linesWeight * candidate.linesWeight +
+							candidate.holesWeight * candidate.holesWeight +
+							candidate.bumpinessWeight * candidate.bumpinessWeight)
+	candidate.heightWeight = candidate.heightWeight / norm
+	candidate.linesWeight = candidate.linesWeight / norm
+	candidate.holesWeight = candidate.holesWeight / norm
+	candidate.bumpinessWeight = candidate.bumpinessWeight / norm
 end
 
 function generateRandomCandidate()
 	math.randomseed(os.clock())
-	candidate = {};
-	candidate.heightWeight: Math.random() - 0.5
-	candidate.linesWeight: Math.random() - 0.5
-	candidate.holesWeight: Math.random() - 0.5,
-	candidate.bumpinessWeight: Math.random() - 0.5
+	candidate = {}
+	candidate.heightWeight: math.random() - 0.5
+	candidate.linesWeight: math.random() - 0.5
+	candidate.holesWeight: math.random() - 0.5,
+	candidate.bumpinessWeight: math.random() - 0.5
 	
-	normalize(candidate);
+	normalize(candidate)
 
-	return candidate;
+	return candidate
 end
 
---Not sure how to do this function at all
 function sort(candidates)
-	candidates.sort(function(a, b)
-	{
-		return b.fitness - a.fitness;
-	});
+	table.sort(candidates, function(a, b)
+		return b.fitness > a.fitness
+	end)
 end
 
 function computeFitnesses(candidates, numberOfGames, maxNumberOfMoves)
-	for i = 0, table.getn(candidates) do
+	for i = 1, table.maxn(candidates) do
 		local candidate = candidates[i]
-		local ai = AI:new{candidate}
+		local ai = AI:new(candidate)
 		local totalScore = 0
 		math.randomseed(os.clock())
 		for j = 0, numberOfGames do
-			local grid = Grid:new{22,10}
+			local grid = Grid:new(18,10)
 			local workingPieces = {Piece.fromIndex(math.random(0,6)),Piece.fromIndex(math.random(0,6))}
-			local workingPiece = workingPiece[0]
+			local workingPiece = workingPiece[1]
 			local score = 0
-			local numberOfMoves = 0
+			local numberOfMoves = 1
 
-			while((numberOfMoves++) < maxNumberOfMoves and not(grid.exceeded()) do
-				workingPiece = ai._best(grid, workingPieces) 
-				while(workingPiece.moveDown(grid)) end
-				grid.addPiece(workingPiece)
-				score += grid.clearLines()
+			while numberOfMoves < maxNumberOfMoves and not grid:exceeded() do
+				workingPiece = ai:best(grid, workingPieces) 
+				while workingPiece:moveDown(grid) do end
+				grid:addPiece(workingPiece)
+				score = score + grid:clearLines()
 
-				for k = 0, k < table.getn(workingPieces)-1 do
-					workingPieces[k] = workingPieces[k+1]
-				end
-				workingPieces[table.getn(workingPieces)-1] = Piece.fromIndex(math.random(0,6)
-				workingPiece = workingPieces[0]
+				workingPieces[1] = workingPieces[2]
+				workingPieces[2] = Piece.fromIndex(math.random(0,6))
+				workingPiece = workingPieces[1]
+				numberOfMoves = numberOfMoves + 1
 			end
-			totalScore += score
+			totalScore = totalScore + score
 		end
 		candidate.fitness = totalScore
 	end
@@ -69,15 +66,15 @@ end
 function tournamentSelectPair(candidates, ways)
 	local indices = {}
 
-	for i = 0, table.getn(candidates) do
+	for i = 1, table.maxn(candidates) do
 		table.insert(indices, i)
 	end 
 
 	local fittestCandidateIndex1 = nil
 	local fittestCandidateIndex2 = nil
 
-	for i = 0, ways do
-		local selectedIndex = indices.splice(randomInteger(0, table.getn(indices), 1)[0] --Not sure of Lua equivalent of splice
+	for i = 1, ways do
+		local selectedIndex = table.remove(indices, randomInteger(1, table.maxn(indices)))
 
 		if fittestCandidateIndex1 == nil or selectedIndex < fittestCandidateIndex1 then
 			fittestCandidateIndex2 = fittestCandidateIndex1
@@ -103,7 +100,7 @@ end
 function mutate(candidate)
 	math.randomseed(os.clock())
 	local quantity = math.random() * .4 - .2
-	local randomInt = randomInteger(0,4)
+	local randomInt = randomInteger(0,3)
 	
 	if randomInt == 0 then
 		candidate.heightWeight += quantity
@@ -117,53 +114,57 @@ function mutate(candidate)
 end
 
 function deleteNLastReplacement(candidates, newCandidates)
-	candidates.splice(-table.getn(newCandidates))
+	for i = 1, table.maxn(newCandidates) do
+		table.remove(candidates)
+	end
 
-	for i = 0, table.getn(newCandidates) do
+	for i = 1, table.maxn(newCandidates) do
 		table.insert(candidates,newCandidates[i])
 	end
 	sort(candidates)
 end
 
-local config = {}
-config.population = 100
-config.rounds = 5
-config.moves = 200
+function tune()
+	local config = {}
+	config.population = 100
+	config.rounds = 5
+	config.moves = 200
 
-local candidates = {}
+	local candidates = {}
 
-for int i = 0, i < config.population do
-	table.insert(candidates, generateRandomCandidate())
-end
+	for i = 1, config.population do
+		table.insert(candidates, generateRandomCandidate())
+	end
 
-print("Computing fitnesses of initial population...")
-computeFitnesses(candidates, config.rounds, config.moves)
-sort(candidates)
+	print("Computing fitnesses of initial population...")
+	computeFitnesses(candidates, config.rounds, config.moves)
+	sort(candidates)
 
-local count = 0
+	local count = 0
 
-while(true) do
-	local newCandidates = {}
+	while(true) do
+		local newCandidates = {}
 
-	for int i = 0, i < 30 do
-		local pair = tournamentSelectPair(candidates,10)
-		local candidate = crossOver(pair[0],pair[1])
-		math.randomseed(os.clock())
-		if(math.random() < 0.05) then
-			mutate(candidate)
+		for int i = 1, 30 do
+			local pair = tournamentSelectPair(candidates,10)
+			local candidate = crossOver(pair[1],pair[2])
+			math.randomseed(os.clock())
+			if(math.random() < 0.05) then
+				mutate(candidate)
+			end
+			normalize(candidate)
+			table.insert(newCandidates, candidate)
 		end
-		normalize(candidate)
-		table.insert(newCandidates, candidate)
-	end
 
-	print("Computing fitnesses of new candidates. (" .. count .. ")")
-	computeFitnesses(newCandidates, config.rounds, config.moves)
-	deleteNLastReplacement(candidates, newCandidates)
-	local totalFitness = 0
-	for int i = 0, i < table.getn(candidates) do
-		totalFitness += candidates[i].fitness
+		print("Computing fitnesses of new candidates. (" .. count .. ")")
+		computeFitnesses(newCandidates, config.rounds, config.moves)
+		deleteNLastReplacement(candidates, newCandidates)
+		local totalFitness = 0
+		for i = 1, table.maxn(candidates) do
+			totalFitness += candidates[i].fitness
+		end
+		console.log("Average fitness = " .. (totalFitness / table.maxn(candidates)))
+		console.log("Highest fitness = " .. candidates[1].fitness .. "(" .. count .. ")")
+		console.log("Fittest candidate = " + candidates[1] + "(" + count + ")") 
 	end
-	console.log("Average fitness = " .. (totalFitness / table.getn(candidates)))
-    console.log("Highest fitness = " .. candidates[0].fitness .. "(" .. count .. ")")
-    console.log("Fittest candidate = " + candidates[0] + "(" + count + ")") 
 end
